a79da1609de74e7ea36300e33753e4305f71a9ba
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-05-18 12:50:50 +0200

    add nyslice data

---
+++ src/thepizzaconundrumproblem/PizzaFromNySliceDataModule.py
@@ -0,0 +1,34 @@
+from ploomberpipelinetemplate.PizzaDataModule import PizzaDto
+
+
+class PizzaFromNySliceDto(PizzaDto):
+    def __init__(
+        self,
+        diameterInCm: float,
+        thicknessInCm: float,
+        priceInRands: float,
+        restaurantName: str = "NY Slice",
+    ):
+        self.RestaurantName = restaurantName
+        self.DiameterInCm = diameterInCm
+        self.ThicknessInCm = thicknessInCm
+        self.PriceInRands = priceInRands
+
+
+_NY_REGULAR_BASE_PIZZA = PizzaFromNySliceDto(
+    diameterInCm=30,
+    thicknessInCm=1,
+    priceInRands=94,
+)
+
+_NY_VERY_LARGE_BASE_PIZZA = PizzaFromNySliceDto(
+    diameterInCm=50,
+    thicknessInCm=1,
+    priceInRands=255,
+)
+
+_NY_ONE_SLICE_BASE_PIZZA = PizzaFromNySliceDto(
+    diameterInCm=50,
+    thicknessInCm=1,
+    priceInRands=34,
+)
