1e94e2dd07c18ee959570edc5f5cf288d358b788
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-05-25 22:44:21 +0200

    Update PizzaFromNySliceDataModule.py

--- src/thepizzaconundrumproblem/PizzaFromNySliceDataModule.py
+++ src/thepizzaconundrumproblem/PizzaFromNySliceDataModule.py
@@ -1,29 +1,21 @@
 from thepizzaconundrumproblem.PizzaDataModule import PizzaDto
 
-def CreatePizzaFromNySliceRegularDto():
-        return PizzaDto(
-            diameterInCm=30,
-            thicknessInCm=1,
-            priceInRands=94,
-            restaurantName="NY Slice",
-        )
+
+class PizzaFromNySliceRegularDto(PizzaDto):
+    def __init__(
+        self,
+    ):
+        self.DiameterInCm = 30
+        self.ThicknessInCm = 1
+        self.PriceInRands = 94
+        self.RestaurantName = "NY Slice"
 
 
 class PizzaFromNySliceLargeBaseDto(PizzaDto):
     def __init__(
         self,
     ):
-        """
-        a
-        """
-        return PizzaDto(
-            diameterInCm=50,
-            thicknessInCm=1,
-            priceInRands=94,
-            restaurantName="NY Slice",
-        )
-
-
-_NY_REGULAR_BASE_PIZZA = CreatePizzaFromNySliceRegularDto()
-
-_NY_VERY_LARGE_BASE_PIZZA = PizzaFromNySliceLargeBaseDto()
+        self.DiameterInCm = 50
+        self.ThicknessInCm = 1
+        self.PriceInRands = 94
+        self.RestaurantName = "NY Slice"
